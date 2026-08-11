9d95fba1c38ea3ed83476484333aca3af649c56f
Jyoti Bhattarai <bhattaraijyoti03@gmail.com>
2026-08-11 21:37:52 +0545

    Add files via upload

---
+++ average of two num..cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	float av;
+	cout<<"Enter first numbers";
+	cin>>a;
+	cout<<"Enter second numbers";
+	cin>>b;
+	cout<<"The average is:"<<(a+b)/2;
+	return 0;
+}
