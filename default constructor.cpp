1765222897e3828b47940a6c348d0d5276b0c7ee
Jyoti Bhattarai <bhattaraijyoti03@gmail.com>
2026-08-19 21:56:46 +0545

    Add default constructor example

---
+++ default constructor.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+class Sample {
+    private:int x,y;
+public:
+   Sample()
+   {
+     x=30;
+     y=50;
+   }
+    void display() {      // function defined inside class
+        cout << "\n x="<< x<<"\n y="<<y;
+    }
+    };
+    int main()
+    {
+        Sample obj1,obj2;
+        obj1.display();
+        obj2.display();
+        return 0;
+    }
